750457ce857d40659229a7304d7e8187d58294b6
Hunter Stevens <hunterstevens1996@gmail.com>
2020-05-20 14:54:57 -0600

    repair method and test working.

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,17 +6,48 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if(!item.durability || !item.name || !item.enhancement){
+    return {...item, message:'This item does not have the credentials to work'};
+  }
+  else if(item.enhancement === 20){
+    return {...item};
+  }
+  else{
+    item.enhancement++;
+    return{...item};
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if(!item.durability || !item.name || !item.enhancement){
+    return {...item, message:'This item does not have the credentials to work'};
+  }
+  
+  else{
+    return {...item};
+  }
 }
 
 function repair(item) {
-  return { ...item };
+  if(!item.durability || !item.name || !item.enhancement){
+    return {...item, message:'This item does not have the credentials to work'};
+  }
+  else if(item.durability < 100){
+    item.durability = 100
+    return { ...item };
+  }
+  else{
+    return {...item};
+  }
+  
 }
 
 function get(item) {
-  return { ...item };
+  if(!item.durability || !item.name || !item.enhancement){
+    return {...item, message:'This item does not have the credentials to work'};
+  }
+  
+  else{
+    return {...item};
+  }
 }
--- enhancing/enhancer.spec.js
+++ enhancing/enhancer.spec.js
@@ -1,2 +1,54 @@
 const enhancer = require('./enhancer.js');
 // test away!
+describe('enhancer', () =>{
+    describe('succeed()', ()=>{
+        test('if the enhancement will work with an item that has not maxed its enhancement', () =>{
+            const initial = {
+                name:'sword',
+                durability:100,
+                enhancement:10
+            };
+            const shouldBE = {
+                name:'sword',
+                durability:100,
+                enhancement:11
+            };
+            expect(enhancer.succeed(initial)).toStrictEqual(shouldBE)
+        })
+        test('if it will return the item untouched if it is already at max enhancement', () =>{
+            const initial = {
+                name:'sword',
+                durability:100,
+                enhancement:20
+            };
+            const shouldBE = {
+                name:'sword',
+                durability:100,
+                enhancement:20
+            };
+            expect(enhancer.succeed(initial)).toStrictEqual(shouldBE)
+        })
+    })
+    describe('fail()', ()=>{
+
+    })
+    describe('get()', ()=>{
+
+    })
+    describe('repair()', ()=>{
+        test('if it will restore durability to one hundred with item less than 100', () =>{
+            const initial = {
+                name:'sword',
+                durability:50,
+                enhancement:20
+            };
+            const shouldBE = {
+                name:'sword',
+                durability:100,
+                enhancement:20
+            }
+            expect(enhancer.repair(initial)).toStrictEqual(shouldBE);
+
+        })
+    })
+})
